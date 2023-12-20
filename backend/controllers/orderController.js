60f24ac886df8318e267d822343403714d7fcaf2
Askinraj <askinraj.com>
2023-12-20 18:58:47 +0530

    20/12/2023 order details

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,75 @@
+const catchAsyncError = require('../middlewares/catchAsyncError')
+const Order = require('../models/orderModel')
+const ErrorHandler = require("../utils/errorHandler")
+
+//Create New Order   -/api/v1/order/new
+exports.newOrder = catchAsyncError(async(req,res,next)=>{
+    const {
+        orderItems,
+        shippingInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paymentInfo
+    }=req.body;
+
+    const order = await Order.create(
+        {
+            orderItems,
+            shippingInfo,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
+            paymentInfo,
+            paidAt:Date.now(),
+            user:req.user.id
+        }
+    )
+
+    res.status(200).json({
+        success:true,
+        order
+    })
+})  
+
+//Get Single Order    -/api/v1/order/:id
+exports.getSingleOrder = catchAsyncError(async(req,res,next)=>{
+const order = await Order.findById(req.params.id).populate('user','name email');
+if(!order)
+{
+    return next(new ErrorHandler(`The Order not found for this id ${req.params.id}`),404)
+}
+
+res.status(200).json({
+    success:true,
+    order
+})
+})
+
+//Get Loggedin User Orders/My Orders     
+
+exports.myOrders = catchAsyncError(async(req,res,next)=>{
+    const orders = await Order.find({user:req.user.id});
+
+    res.status(200).json({
+        success:true,
+        orders
+    })
+})
+
+
+//admin: Get All Orders
+
+exports.orders = catchAsyncError(async(req,res,next)=>{
+    const orders = await Order.find();
+    var totalAmount = 0;
+    orders.forEach((order)=>{totalAmount += order.totalPrice});
+    res.status(200).json({
+        success:true,
+        orders,
+        totalAmount
+    })
+})
+
